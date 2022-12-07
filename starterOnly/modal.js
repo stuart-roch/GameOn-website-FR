19569ed0713f502d96c35d06557cd2916f795b92
Stuart ROCH <stuart.roch@hotmail.fr>
2022-12-07 12:51:59 +0100

    affichage message de confirmation si formulaire valide

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -2,7 +2,7 @@
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalBtnClose = document.querySelector(".close");
+const modalClose = document.querySelector(".close");
 const form = document.querySelector("form");
 
 // DOM Elements from form
@@ -54,75 +54,9 @@ function closeModal(){
   document.querySelector(".hero-section").classList.toggle("modal-open");
   document.querySelector(".footer").classList.toggle("modal-open");
   document.querySelector("main").classList.toggle("modal-open");
-
-  /*document.querySelector(".topnav").removeAttribute("style");
-  document.querySelector(".hero-section").removeAttribute("style");
-  document.querySelector("footer").removeAttribute("style");*/
 }
 
-function firstNameIsValid(){
-  if (!(/^[a-zA-Z]{2,}/.test(firstName.value))) {
-    formData[0].setAttribute("data-error","Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-    formData[0].setAttribute("data-error-visible","true");
-    return false;
-  }
-  else{
-    formData[0].removeAttribute("data-error");
-    formData[0].removeAttribute("data-error-visible");
-    return true;
-  }
-}
-function lastNameIsValid(){
-  if (!(/^[a-zA-Z]{2,}/.test(lastName.value))) {
-    formData[1].setAttribute("data-error","Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-    formData[1].setAttribute("data-error-visible","true");
-    return false;
-  }
-  else{
-    formData[1].removeAttribute("data-error");
-    formData[1].removeAttribute("data-error-visible");
-    return true;
-  }
-}
 
-function emailIsValid(){
-  if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))) {
-    formData[2].setAttribute("data-error","Veuillez entrer une adresse mail valide.");
-    formData[2].setAttribute("data-error-visible","true");
-    return false;
-  }
-  else{
-    formData[2].removeAttribute("data-error");
-    formData[2].removeAttribute("data-error-visible");
-    return true;
-  }
-}
-
-function birthDateIsValid(){
-  if(!(/^(19[0-9]{2}|202[0-3]|20[0-1][0-9])[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/.test(birthDate.value))) {
-    formData[3].setAttribute("data-error","Veuillez entrer une date de naissance valide.");
-    formData[3].setAttribute("data-error-visible","true");
-    return false;
-  }
-  else{
-    formData[3].removeAttribute("data-error");
-    formData[3].removeAttribute("data-error-visible");
-    return true;
-  }
-}
-
-function numberTournamentsIsValid(){
-  if (!(/^[0-9]{1,}/.test(numberTournaments.value))){
-    formData[4].setAttribute("data-error","Veuillez entrer une valeur numérique.");
-    formData[4].setAttribute("data-error-visible","true");
-    return false;
-  }
-  else{
-    formData[4].removeAttribute("data-error");
-    formData[4].removeAttribute("data-error-visible");
-    return true;
-  }
-}
 function locationIsValid(){
   let checked=false;
   for (let i=0;i<listLocation.length;i++){
@@ -157,21 +91,7 @@ function termsIsValid(){
 // test if all input are valid
 function modalIsValid(){
   let cpt=0;
-  /*if(firstNameIsValid()){
-    cpt++;
-  }
-  if(lastNameIsValid()){
-    cpt++;
-  }
-  if(emailIsValid()){
-    cpt++;
-  }
-  if(birthDateIsValid()){
-    cpt++;
-  }
-  if(numberTournamentsIsValid()){
-    cpt++;
-  }*/
+  
   fields.forEach(field => {if(field.regex.test(field.input.value)){
     field.isValid=true;
     cpt++;
@@ -195,12 +115,23 @@ function displayErrorMsg(){
   }})
 }
 function displayConfirmationMsg(){
-  document.querySelector(".modal-body").innerHTML="<p> Merci pour votre inscription ! </p>";
-  document.querySelector(".modal-body").style.textAlign="center";
+  document.querySelector(".msg-confirmation").classList.toggle("form-submitted");
+  document.querySelector(".button-close").classList.toggle("form-submitted");
+  form.style.display="none";
+  document.querySelector(".modal-body").classList.toggle("form-submitted");
 }
+
+function closeConfirmationMsg(){
+  document.querySelector(".msg-confirmation").classList.toggle("form-submitted");
+  document.querySelector(".button-close").classList.toggle("form-submitted");
+  form.style.display="block";
+  document.querySelector(".modal-body").classList.toggle("form-submitted");
+  closeModal();
+}
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalBtnClose.addEventListener("click",closeModal);
+modalClose.addEventListener("click",closeModal);
 
 form.addEventListener("submit",function(e){
   e.preventDefault();
@@ -209,6 +140,5 @@ form.addEventListener("submit",function(e){
   }else{
     displayErrorMsg();
   }});
-/*btnSubmit.addEventListener("click",function(e){
-  e.preventDefault();
-})*/
+
+document.querySelector(".button-close").addEventListener("click",closeConfirmationMsg);
